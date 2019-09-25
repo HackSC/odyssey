c27f64c0a9533839db217c56e6110985277547a8
Wilhelm Willie <WilhelmWillie@gmail.com>
2019-09-24 21:59:44 -0700

    [Setup] Use Sequelize as MySQL ORM (#3)
    * Initial commit for sequelize-cli init
    
    * Configured sequelize
    
    * First migration for User model
    
    * ES6'd migration and models file
    
    * Quick update to config for local dev
    
    * Wrote documentation for Sequelize

---
+++ server/src/migrations/20190925002329-create-user.js
@@ -0,0 +1,41 @@
+export default {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      jwt: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      verifyCode: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
