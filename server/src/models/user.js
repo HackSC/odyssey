83cc6cf76669b9fca74cf873c8ee5734118da2c9
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-09-24 17:24:15 -0700

    First migration for User model

---
+++ server/src/models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    jwt: DataTypes.STRING,
+    status: DataTypes.STRING,
+    role: DataTypes.STRING,
+    verifyCode: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
