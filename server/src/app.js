5c49f1d5c1b5bcdfdc1c63782a94967f3248c25e
saresend <saresend@usc.edu>
2019-10-01 13:23:51 -0700

    configuring express-session for dev

--- server/src/app.js
+++ server/src/app.js
@@ -4,10 +4,21 @@
 
 import express from "express";
 import bodyParser from "body-parser";
+import session from 'express-session';
+import dotenv from 'dotenv';
 
 import api from "./routes/api";
 
+dotenv.config();
+
 const app = express();
+
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+};
 
 app.use(bodyParser.json());
 
@@ -17,6 +28,7 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use(session(sessionConfig));
 app.use("/api", api);
 
 export default app;
