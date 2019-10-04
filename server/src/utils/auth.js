6691980bc61163c48f1031cfa2b12293c82afacf
saresend <saresend@usc.edu>
2019-10-03 20:00:50 -0700

    adjusts audience param so now authentication works

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -1,16 +1,15 @@
-import jwt from 'express-jwt';
-import jwksRsa from 'jwks-rsa';
+import jwt from "express-jwt";
+import jwksRsa from "jwks-rsa";
 
-import {isDevEnvType } from './server';
-
+import { isDevEnvType } from "./server";
 
 const getDevParams = () => {
   // Remote resources owned by: saresend@usc.edu
-  return  {
-    jwksUri: 'https://dev-l4sg3wav.auth0.com/.well-known/jwks.json',
-    audience: 'https://localhost',
-    domain: 'https://dev-l4sg3wav.auth0.com',
-    client_id: 'ICCkgINzCPDq66k7nuFmdrFwEjt2Uv8f',
+  return {
+    jwksUri: "https://dev-l4sg3wav.auth0.com/.well-known/jwks.json",
+    audience: "https://dev-l4sg3wav.auth0.com/api/v2/",
+    domain: "https://dev-l4sg3wav.auth0.com",
+    client_id: "ICCkgINzCPDq66k7nuFmdrFwEjt2Uv8f"
   };
 };
 
@@ -23,13 +22,12 @@ const createJwtVerifierForEnv = envType => {
       cache: true,
       rateLimit: true,
       jwksRequestsPerMinute: 50,
-      jwksUri: isDev ? devParams.jwksUri : process.env.JWKS_URI,
+      jwksUri: isDev ? devParams.jwksUri : process.env.JWKS_URI
     }),
     audience: isDev ? devParams.audience : process.env.AUDIENCE,
-    issuer:  isDev ? devParams.issuer : process.env.DOMAIN,
-    algorithms: ['RS256'],
-  })
+    issuer: isDev ? devParams.issuer : process.env.DOMAIN,
+    algorithms: ["RS256"]
+  });
 };
-
 
 export { createJwtVerifierForEnv, getDevParams };
