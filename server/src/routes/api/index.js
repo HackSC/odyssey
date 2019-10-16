92dc080bcafed4f13669d786c38aeda2467a8259
saresend <saresend@usc.edu>
2019-10-15 17:37:08 -0700

    adds back dummy routes for test passing and server sanity checking

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import users from "./users";
 import hackerProfile from "./hackerProfile";
+import testRoutes from "./testRoutes";
 
 const router = express.Router();
 
 router.use("/users", users);
 router.use("/hackerProfiles", hackerProfile);
+router.use("/", testRoutes);
 
 export default router;
