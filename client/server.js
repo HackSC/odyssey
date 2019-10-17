83ca41158ffdfd15c9de14b8dab6fc38998c1fcf
saresend <saresend@usc.edu>
2019-10-16 17:51:33 -0700

    now has passport implementation

--- client/server.js
+++ client/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const http = require("http");
 const next = require("next");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({
@@ -9,10 +13,41 @@ const app = next({
 });
 const handle = app.getRequestHandler();
 
+dotenv.config();
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // extraParams.id_token should contain the JWT
+    return done(null, profile);
+  }
+);
+
+const sessionConfig = {
+  secret: "CHANGE THIS TO RANDOM",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+passport.use(strategy);
+
 app.prepare().then(() => {
   const server = express();
   // handling everything else with Next.js
   server.get("*", handle);
+
+  // Authentication config
+  server.use(session(sessionConfig));
+  server.use(passport.initialize());
+  server.use(passport.session());
+
   const port_num = 3000;
   http.createServer(server).listen(port_num, () => {
     console.log(`listening on port ${port_num}`);
