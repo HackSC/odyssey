8b90a16f0f293e26d62546a2c15ac8d114a3294b
saresend <saresend@usc.edu>
2019-10-17 17:49:12 -0700

    adds hackerProfile

---
+++ client/api/models/hackerProfile.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const HackerProfile = sequelize.define(
+    "HackerProfile",
+    {
+      gender: {
+        type: DataTypes.ENUM,
+        values: ["male", "female", "other"]
+      },
+      ethnicity: DataTypes.STRING,
+      major: DataTypes.STRING,
+      minor: DataTypes.STRING,
+      resume: DataTypes.STRING,
+      skills: DataTypes.STRING,
+      interests: DataTypes.STRING
+    },
+    {}
+  );
+  HackerProfile.associate = function(models) {
+    HackerProfile.belongsTo(models.User);
+
+    // associations can be defined here
+  };
+  return HackerProfile;
+};
