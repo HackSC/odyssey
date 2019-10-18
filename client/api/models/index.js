4fbd91f51efb489f310f8f3f54a479b775ddbbf6
saresend <saresend@usc.edu>
2019-10-17 17:19:03 -0700

    now does users, sorta?

--- client/api/models/index.js
+++ client/api/models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/database.js")[env];
+const config = require(__dirname + "/../database/database.js")[env];
 const db = {};
 
 let sequelize;
@@ -19,3 +19,25 @@ if (config.use_env_variable) {
     config
   );
 }
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach(file => {
+    const model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
