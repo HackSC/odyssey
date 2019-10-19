031091fe3e2f8e0fc1234c0bf49032d7bc16315c
saresend <saresend@usc.edu>
2019-10-18 23:47:39 -0700

    prettier

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -33,12 +33,12 @@ const formSteps: FormStep[] = [
 
 const Dashboard = initialObject => {
   const { user } = initialObject;
-	const [ newProfile, setNewProfile ] = useState({
-		gender: 'other',
-		major: 'computer science',
-		skills: 'none really',
-		interests: 'snowboarding',
-	});
+  const [newProfile, setNewProfile] = useState({
+    gender: "other",
+    major: "computer science",
+    skills: "none really",
+    interests: "snowboarding"
+  });
   return (
     <Layout>
       <div>
@@ -47,24 +47,27 @@ const Dashboard = initialObject => {
         <div>Username: {user.nickname} </div>
         <div> Email Verified: {user._json.email_verified + ""} </div>
         <div>
-					<div> Create a user profile with the button below! </div>
-		<div>
-			<textarea value={JSON.stringify(newProfile)} onChange={(e) => {
-				setNewProfile(JSON.parse(e.target.value));	
-			}}>
-			{	JSON.stringify(newProfile) }
-		</textarea>
-	</div>
+          <div> Create a user profile with the button below! </div>
+          <div>
+            <textarea
+              value={JSON.stringify(newProfile)}
+              onChange={e => {
+                setNewProfile(JSON.parse(e.target.value));
+              }}
+            >
+              {JSON.stringify(newProfile)}
+            </textarea>
+          </div>
           <button
-						onClick={async () => {
-							const profileData = JSON.stringify(newProfile);
-							console.log(profileData);
+            onClick={async () => {
+              const profileData = JSON.stringify(newProfile);
+              console.log(profileData);
               const res = await fetch("api/profile", {
-								method: "POST",
-								headers : {
-										'Content-Type': 'application/json'
-								},
-								body: profileData 
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: profileData
               });
               console.log(res);
             }}
