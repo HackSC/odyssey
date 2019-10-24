1a5888927af06ca5b7ecc7a8edf5820e3f36813d
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-23 21:22:27 -0700

    Added more styled components

--- client/pages/sample.tsx
+++ client/pages/sample.tsx
@@ -5,95 +5,115 @@ import styled from "styled-components";
 const Sample = () => {
   return (
     <>
-      <h1>Heading 1</h1>
+      <Container>
+        <Section>
+          <h1>Heading 1</h1>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et justo
-        eleifend, hendrerit quam eget, condimentum dui. Aliquam vel quam ut
-        dolor dignissim volutpat luctus in lorem. Vestibulum ante ipsum primis
-        in faucibus orci luctus et ultrices posuere cubilia Curae;
-      </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+            justo eleifend, hendrerit quam eget, condimentum dui. Aliquam vel
+            quam ut dolor dignissim volutpat luctus in lorem. Vestibulum ante
+            ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+            Curae;
+          </p>
+        </Section>
 
-      <h2>Heading 2</h2>
+        <Section>
+          <h2>Heading 2</h2>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et justo
-        eleifend, hendrerit quam eget, condimentum dui. Aliquam vel quam ut
-        dolor dignissim volutpat luctus in lorem.
-      </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+            justo eleifend, hendrerit quam eget, condimentum dui. Aliquam vel
+            quam ut dolor dignissim volutpat luctus in lorem.
+          </p>
+        </Section>
 
-      <h3>Heading 3</h3>
+        <Panel>
+          <h3>Heading 3</h3>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et justo
-        eleifend, hendrerit quam eget, condimentum dui.
-      </p>
-
-      <Flex>
-        <Column>
-          <h2>Equal width columns</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
             justo eleifend, hendrerit quam eget, condimentum dui.
           </p>
-        </Column>
+        </Panel>
 
-        <Column>
-          <h2>Equal width columns</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
-            justo eleifend, hendrerit quam eget, condimentum dui.
-          </p>
-        </Column>
-      </Flex>
+        <Section>
+          <Flex>
+            <Column>
+              <h2>Equal width columns</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+                justo eleifend, hendrerit quam eget, condimentum dui.
+              </p>
+            </Column>
 
-      <Flex>
-        <Column>
-          <h3>Equal width columns</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
-            justo eleifend, hendrerit quam eget, condimentum dui.
-          </p>
-        </Column>
+            <Column>
+              <h2>Equal width columns</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+                justo eleifend, hendrerit quam eget, condimentum dui.
+              </p>
+            </Column>
+          </Flex>
+        </Section>
 
-        <Column>
-          <h3>Equal width columns</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
-            justo eleifend, hendrerit quam eget, condimentum dui.
-          </p>
-        </Column>
+        <Section>
+          <Flex>
+            <Column>
+              <h3>Equal width columns</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+                justo eleifend, hendrerit quam eget, condimentum dui.
+              </p>
+            </Column>
 
-        <Column>
-          <h3>Equal width columns</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
-            justo eleifend, hendrerit quam eget, condimentum dui.
-          </p>
-        </Column>
-      </Flex>
+            <Column>
+              <h3>Equal width columns</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+                justo eleifend, hendrerit quam eget, condimentum dui.
+              </p>
+            </Column>
 
-      <form>
-        <FormGroup>
-          <label>Name</label>
-          <input type="text" />
-        </FormGroup>
+            <Column>
+              <h3>Equal width columns</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+                justo eleifend, hendrerit quam eget, condimentum dui.
+              </p>
+            </Column>
+          </Flex>
+        </Section>
 
-        <FormGroup>
-          <label>Title</label>
-          <input type="text" />
-        </FormGroup>
+        <form>
+          <FormGroup>
+            <label>Name</label>
+            <input type="text" />
+          </FormGroup>
 
-        <FormGroup>
-          <label>Why HackSC?</label>
-          <textarea />
-        </FormGroup>
+          <FormGroup>
+            <label>Title</label>
+            <input type="text" />
+          </FormGroup>
 
-        <Button>Submit</Button>
-      </form>
+          <FormGroup>
+            <label>Why HackSC?</label>
+            <textarea />
+          </FormGroup>
+
+          <Button>Submit</Button>
+        </form>
+      </Container>
     </>
   );
 };
+
+const Container = styled.div`
+  width: 93.75%;
+  max-width: 960px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const Flex = styled.div`
   display: flex;
@@ -101,6 +121,19 @@ const Flex = styled.div`
 
 const Column = styled.div`
   flex: 1;
+`;
+
+const Section = styled.div`
+  padding: 32px 0;
+`;
+
+const Panel = styled.div`
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  box-sizing: border-box;
+  padding: 36px;
 `;
 
 const FormGroup = styled(Flex)`
