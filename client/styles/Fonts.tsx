d5e91174195d3d744c037b6997963feb64a423d5
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-23 20:53:38 -0700

    No plan to use Proggy in Odyssey

--- client/styles/Fonts.tsx
+++ client/styles/Fonts.tsx
@@ -21,13 +21,6 @@ const Fonts = css`
     font-weight: 700;
     font-style: normal;
   }
-
-  @font-face {
-    font-family: "Proggy";
-    src: url("/fonts/proggy.woff") format("woff");
-    font-weight: 400;
-    font-style: normal;
-  }
 `;
 
 export default Fonts;
