019bf0794ced8ce119a397ef5c261f2d919fc617
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-28 09:32:29 -0700

    Basic form setup for profile

--- components/applicationSteps/ProfileStep.tsx
+++ components/applicationSteps/ProfileStep.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 
+import { Form, FormGroup, Button } from "../../styles";
+
 type Props = {
-  user: any;
+  user: User;
 };
 
 const ProfileStep: React.FunctionComponent<Props> = props => {
@@ -15,6 +17,34 @@ const ProfileStep: React.FunctionComponent<Props> = props => {
         This is a component that will let the user fill out their hacker
         profile. This will include a form where they can indicate their
         interests, skills, resume, and more.
+        <Form>
+          <FormGroup>
+            <label>First Name</label>
+            <input type="text" />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Last Name</label>
+            <input type="text" />
+          </FormGroup>
+
+          <FormGroup>
+            <label>E-Mail</label>
+            <input type="text" />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Major</label>
+            <input type="text" />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Ethnicity</label>
+            <input type="text" />
+          </FormGroup>
+
+          <Button>Submit</Button>
+        </Form>
       </p>
     </div>
   );
