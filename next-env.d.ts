889b59c0ec6a46e6bea44416f4ab93ebd5255246
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-27 21:43:02 -0700

    Added the <any> back to the FormStep prop type

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,7 +4,7 @@
 // Not really sure what to do with types
 declare type FormStep = {
   title: string;
-  component: React.FunctionComponent;
+  component: React.FunctionComponent<any>;
 };
 
 declare module "*.png";
