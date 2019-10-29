5e906d7f06383733e5032da4151296e038345b8e
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-29 11:37:41 -0700

    Updating hackerProfile model to reflect changes to schema from migrations

--- api/models/hackerProfile.js
+++ api/models/hackerProfile.js
@@ -13,7 +13,38 @@ module.exports = (sequelize, DataTypes) => {
       minor: DataTypes.STRING,
       resume: DataTypes.STRING,
       skills: DataTypes.STRING,
-      interests: DataTypes.STRING
+      interests: DataTypes.STRING,
+      applicationSubmittedAt: DataTypes.DATE,
+      profileSubmittedAt: DataTypes.DATE,
+      status: {
+        type: DataTypes.ENUM,
+        values: [
+          "unverified",
+          "verified",
+          "profileSubmitted",
+          "applicationSubmitted",
+          "accepted",
+          "waitlisted",
+          "rejected",
+          "confirmed",
+          "checkedIn"
+        ]
+      },
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      phoneNumber: DataTypes.STRING,
+      school: DataTypes.STRING,
+      year: {
+        type: DataTypes.ENUM,
+        values: ["Freshman", "Sophomore", "Junior", "Senior", "Graduate"]
+      },
+      skillLevel: {
+        type: DataTypes.ENUM,
+        values: ["Beginner", "Intermediate", "Advanced"]
+      },
+      questionOne: DataTypes.STRING(1000),
+      questionTwo: DataTypes.STRING(1000),
+      questionThree: DataTypes.STRING(1000)
     },
     {}
   );
