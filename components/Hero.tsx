76ef078c9fb0b3be57c1192296cb280b7d41309e
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-31 20:48:21 -0700

    Blob home page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,111 @@
+import React, { useEffect } from "react";
+
 import styled from "styled-components";
 
-const Hero = () => (
-  <Wrapper>
-    <h1>Apply to HackSC 2020</h1>
+import * as THREE from "three";
+import SimplexNoise from "simplex-noise";
 
-    <p>
-      HackSC is the University of Southern California’s largest hackathon. This
-      year, we’re focused on bringing real solutions to those in need by pushing
-      the boundaries of technology, innovation, and collaboration.
-    </p>
+import { Flex, Column } from "../styles";
 
-    <br />
+const Hero = () => {
+  useEffect(() => {
+    let $canvas = document.querySelector("#blob canvas"),
+      canvas: any = $canvas,
+      renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        context: canvas.getContext("webgl2"),
+        antialias: true,
+        alpha: true
+      }),
+      simplex = new SimplexNoise();
 
-    <p>
-      TODO: Style this landing page with an attractive hero component that
-      entices people to sign up and apply
-    </p>
-  </Wrapper>
-);
+    renderer.setSize($canvas.clientWidth, $canvas.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio || 1);
+
+    let scene = new THREE.Scene(),
+      camera = new THREE.PerspectiveCamera(
+        30,
+        $canvas.clientWidth / $canvas.clientHeight,
+        0.1,
+        500
+      );
+
+    camera.position.z = 5;
+
+    let geometry = new THREE.SphereGeometry(0.8, 128, 128);
+
+    let material = new THREE.MeshPhongMaterial({
+      color: 0xff8379,
+      shininess: 40
+    });
+
+    let lightTop = new THREE.DirectionalLight(0xffffff, 0.55);
+    lightTop.position.set(0, 500, 200);
+    lightTop.castShadow = true;
+    scene.add(lightTop);
+
+    let lightBottom = new THREE.DirectionalLight(0xfeda22, 0.8);
+    lightBottom.position.set(0, -500, 400);
+    lightBottom.castShadow = true;
+    scene.add(lightBottom);
+
+    let ambientLight = new THREE.AmbientLight(0x86dcea);
+    scene.add(ambientLight);
+
+    let sphere: any = new THREE.Mesh(geometry, material);
+
+    scene.add(sphere);
+
+    let update = () => {
+      let time = performance.now() * 0.00001 * 13 * Math.pow(1, 3),
+        spikes = 0.6 * 1;
+
+      for (let i = 0; i < sphere.geometry.vertices.length; i++) {
+        let p = sphere.geometry.vertices[i];
+        p.normalize().multiplyScalar(
+          1 +
+            0.3 *
+              simplex.noise3D(p.x * spikes, p.y * spikes, p.z * spikes + time)
+        );
+      }
+
+      sphere.geometry.computeVertexNormals();
+      sphere.geometry.normalsNeedUpdate = true;
+      sphere.geometry.verticesNeedUpdate = true;
+    };
+
+    function animate() {
+      update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+
+    requestAnimationFrame(animate);
+  }, []);
+
+  return (
+    <Wrapper>
+      <Flex align="center">
+        <Column flexBasis={49}>
+          <Blob id="blob">
+            <canvas></canvas>
+          </Blob>
+        </Column>
+
+        <Column flexBasis={49}>
+          <h1>Apply to HackSC</h1>
+
+          <p>
+            HackSC is the University of Southern California’s largest hackathon.
+            This year, we’re focused on bringing real solutions to those in need
+            by pushing the boundaries of technology, innovation, and
+            collaboration.
+          </p>
+        </Column>
+      </Flex>
+    </Wrapper>
+  );
+};
 
 // TODO: Flesh this out and make it pretty
 const Wrapper = styled.div`
@@ -25,6 +113,22 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   height: 600px;
+
+  h1 {
+    font-size: ${({ theme }) => theme.fontSizes.large};
+  }
+
+  p {
+    line-height: 28px;
+  }
+`;
+
+const Blob = styled.div`
+  position: relative;
+  canvas {
+    width: 500px;
+    height: 500px;
+  }
 `;
 
 export default Hero;
