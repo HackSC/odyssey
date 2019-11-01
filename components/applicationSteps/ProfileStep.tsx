e9ef800fdcace0c3f7af0e30c97735d8cb83122e
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-31 17:10:42 -0700

    Switch order of male + female (more males apply to hackathons, should be default)

--- components/applicationSteps/ProfileStep.tsx
+++ components/applicationSteps/ProfileStep.tsx
@@ -46,8 +46,8 @@ const gradDateOptions = [
 ];
 
 const genderOptions = [
-  { label: "Female", value: "female" },
   { label: "Male", value: "male" },
+  { label: "Female", value: "female" },
   { label: "Non-Binary", value: "non-binary" },
   { label: "Other", value: "other" },
   { label: "Prefer not to say", value: "no-say" }
