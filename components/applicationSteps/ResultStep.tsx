172eb9c27c22a8541e6b92c5a42b6ad6b2045b6b
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-10-31 19:52:35 -0700

    Delete old results step code to make way for future PRs

--- components/applicationSteps/ResultStep.tsx
+++ components/applicationSteps/ResultStep.tsx
@@ -5,19 +5,7 @@ type Props = {
 };
 
 const ResultStep: React.FunctionComponent<Props> = props => {
-  const { user } = props;
-
-  return (
-    <div>
-      <h1>Hello {user.displayName}</h1>
-
-      <p>
-        This is a component that will represent the user's application results.
-        If the user is accepted, they will see a confirmation form. Otherwise,
-        they will be rejected.
-      </p>
-    </div>
-  );
+  return <div>Results</div>;
 };
 
 export default ResultStep;
