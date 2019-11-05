5d12cc9c881ebf55baaa4f9ba4f7dc6e234fe7eb
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-11-05 13:02:07 -0800

    Improved status check, in case status doesn't update properly

--- components/steps/Status.tsx
+++ components/steps/Status.tsx
@@ -23,12 +23,13 @@ const getStatusLabel = (status: string) => {
   return status[0].toUpperCase() + status.substring(1, status.length);
 };
 
-const getStage = (status: string): number => {
-  if (status === "unverified") {
+const getStage = (profile: Profile): number => {
+  if (profile.status === "unverified") {
     return 1;
-  } else if (status === "verified") {
+  } else if (profile.status === "verified" && !profile.submittedAt) {
     return 2;
   }
+
   return 3;
 };
 
@@ -59,21 +60,21 @@ const StatusStep: React.FunctionComponent<Props> = props => {
         <Column flexBasis={63}>
           <h2>Next Steps</h2>
           <Steps>
-            <Step disabled={getStage(status) > 1}>
+            <Step disabled={getStage(profile) > 1}>
               <h3>1. Verify your e-mail</h3>
               <p>
                 Make sure to check your e-mail and verify your account. If you
                 run into issues, log-out and log back in.
               </p>
             </Step>
-            <Step disabled={getStage(status) > 2}>
+            <Step disabled={getStage(profile) > 2}>
               <h3>2. Fill out an application</h3>
               <p>
                 Answer a few questions to show why you want to be at HackSC
                 2020!
               </p>
 
-              {getStage(status) === 2 && (
+              {getStage(profile) === 2 && (
                 <StepButton onClick={() => navigateTo("application")}>
                   Fill out application
                 </StepButton>
@@ -82,7 +83,7 @@ const StatusStep: React.FunctionComponent<Props> = props => {
             <Step>
               <h3>3. View Results</h3>
               <p>Come back soon and see your results.</p>
-              {getStage(status) === 3 && (
+              {getStage(profile) === 3 && (
                 <StepButton onClick={() => navigateTo("results")}>
                   View Results
                 </StepButton>
