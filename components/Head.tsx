5232da4f1854fc8ba59c95e15caef742daf20aea
John Zeiders <zeiders@usc.edu>
2019-11-11 16:24:57 -0800

    Adds Facebook tracking pixel

--- components/Head.tsx
+++ components/Head.tsx
@@ -16,8 +16,25 @@ const Head = ({ title }: HeadProps) => {
 
       <script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=UA-127488741-2"
-      ></script>
+        src="https://www.googletagmanager.com/gtag/js?id=UA-127488741-2"></script>
+
+      {/* Facebook pixel (come get my data zuck) */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '2475646642710223');
+  fbq('track', 'PageView');`
+        }}
+      />
+
       <script
         dangerouslySetInnerHTML={{
           __html: `
