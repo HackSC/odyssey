a5f7c84cee7521ad2a9ee9434426a74b7180d18a
saresend <saresend@usc.edu>
2019-11-14 17:22:59 -0800

    successfully can run migration

---
+++ api/database/migrations/20191115010855-add-review-table.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("HackerReviews", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      hackerId: {
+        type: Sequelize.STRING,
+        references: {
+          model: "HackerProfiles",
+          key: "userId"
+        }
+      },
+      createdBy: {
+        // Should reference admin who created this review
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: "HackerProfiles",
+          key: "userId"
+        }
+      },
+      scoreOne: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      scoreTwo: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      scoreThree: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      comments: {
+        allowNull: true,
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("HackerReviews");
+  }
+};
