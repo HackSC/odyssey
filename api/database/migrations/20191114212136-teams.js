8123139c71dc0f7056f96cff880e48fe1efdb1f5
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-11-23 00:05:54 -0800

    fixes possible race condition for migrations

--- api/database/migrations/20191114212136-teams.js
+++ api/database/migrations/20191114212136-teams.js
@@ -2,13 +2,8 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-<<<<<<< HEAD
     return queryInterface
       .createTable("Teams", {
-=======
-    return Promise.all([
-      queryInterface.createTable("Teams", {
->>>>>>> Set up migrations and model for teams
         id: {
           allowNull: false,
           type: Sequelize.INTEGER,
