376f68934db20d2c7f17c11e75123662b7fa77e6
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-12-10 13:21:01 -0800

    spruced up app review page

--- pages/appReview.tsx
+++ pages/appReview.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import {
   handleLoginRedirect,
@@ -10,7 +11,7 @@ import { getHackerProfileForReview, submitReview } from "../lib/admin";
 import Head from "../components/Head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Button, Container, Background } from "../styles";
+import { Button, Container, Background, Flex, Column } from "../styles";
 
 const AppReview = ({ hackerProfile, review }) => {
   console.log(hackerProfile);
@@ -25,58 +26,101 @@ const AppReview = ({ hackerProfile, review }) => {
     <>
       <Head title="HackSC Odyssey - Application" />
       <Navbar loggedIn admin activePage="/" />
-      <Container>
-        <Background>
-          <h1> Applicant Info </h1>
-          <h2> Question 1 </h2>
-          <p> {hackerProfile.questionOne} </p>
-          <h2> Question 2 </h2>
-          <p> {hackerProfile.questionTwo} </p>
-          <h2> Question 3 </h2>
-          <p> {hackerProfile.questionThree} </p>
-        </Background>
-        <Background>
-          <h1> Review </h1>
-          <h3> Score 1 </h3>
-          <input
-            type="number"
-            onChange={e => {
-              setS1(e.target.value);
-            }}
-          />
-          <h3> Score 2 </h3>
-          <input
-            type="number"
-            onChange={e => {
-              setS2(e.target.value);
-            }}
-          />
-          <h3> Score 3 </h3>
-          <input
-            type="number"
-            onChange={e => {
-              setS3(e.target.value);
-            }}
-          />
-          <div>
-            <button
-              onClick={async () => {
-                const result = await submitReview(review, {
-                  scoreOne: s1,
-                  scoreTwo: s2,
-                  scoreThree: s3
-                });
-                // Redirect back to admin from the server
-                handleAdminRedirect(null);
-              }}
-            >
-              {" "}
-              Submit Review{" "}
-            </button>
-          </div>
-        </Background>
-      </Container>
+      <Background>
+        <Container>
+          <Flex direction="row" justify="space-between">
+            <Column flexBasis={48}>
+              <h1> Applicant Info </h1>
 
+              <Panel>
+                <h2>Question 1 - Project</h2>
+                <p> {hackerProfile.questionOne || "(No response)"} </p>
+              </Panel>
+
+              <Panel>
+                <h2>Question 2 - Vertical</h2>
+                <p> {hackerProfile.questionTwo || "(No response)"} </p>
+              </Panel>
+
+              <Panel>
+                <h2>Question 3 - Beverage</h2>
+                <p> {hackerProfile.questionThree || "(No response)"} </p>
+              </Panel>
+            </Column>
+
+            <Column flexBasis={48}>
+              <h1>Review</h1>
+              <Panel>
+                <Flex direction="row" justify="space-between" align="center">
+                  <Column>
+                    <ScoreInputLabel>Score 1</ScoreInputLabel>
+                  </Column>
+
+                  <Column flexGrow={1}>
+                    <Input
+                      type="number"
+                      onChange={e => {
+                        setS1(e.target.value);
+                      }}
+                    />
+                  </Column>
+                </Flex>
+              </Panel>
+
+              <Panel>
+                <Flex direction="row" justify="space-between" align="center">
+                  <Column>
+                    <ScoreInputLabel>Score 2</ScoreInputLabel>
+                  </Column>
+
+                  <Column flexGrow={1}>
+                    <Input
+                      type="number"
+                      onChange={e => {
+                        setS2(e.target.value);
+                      }}
+                    />
+                  </Column>
+                </Flex>
+              </Panel>
+
+              <Panel>
+                <Flex direction="row" justify="space-between" align="center">
+                  <Column>
+                    <ScoreInputLabel>Score 3</ScoreInputLabel>
+                  </Column>
+
+                  <Column flexGrow={1}>
+                    <Input
+                      type="number"
+                      onChange={e => {
+                        setS3(e.target.value);
+                      }}
+                    />
+                  </Column>
+                </Flex>
+              </Panel>
+              <div>
+                <Button
+                  onClick={async e => {
+                    e.preventDefault();
+                    const result = await submitReview(review, {
+                      scoreOne: s1,
+                      scoreTwo: s2,
+                      scoreThree: s3
+                    });
+                    // Redirect back to admin from the server
+                    handleAdminRedirect(null);
+                  }}
+                >
+                  {" "}
+                  Submit Review{" "}
+                </Button>
+              </div>
+            </Column>
+          </Flex>
+        </Container>
+      </Background>
       <Footer />
     </>
   );
@@ -97,5 +141,28 @@ AppReview.getInitialProps = async ctx => {
     review: profileReview.review
   };
 };
+
+const Panel = styled.div`
+  padding: 24px 36px;
+  margin: 0 0 16px;
+  background: #ffffff;
+  border-radius: 4px;
+`;
+
+const Input = styled.input`
+  border-radius: 8px;
+  border: 1px solid #b2b2b2;
+  padding: 12px 16px;
+  font-weight: 300;
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 16px;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+const ScoreInputLabel = styled.h3`
+  padding: 0;
+  margin-right: 32px;
+`;
 
 export default AppReview;
