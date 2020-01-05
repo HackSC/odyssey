27c99d3e0b4e9547a17f165a8a88c205e0e10572
saresend <saresend@usc.edu>
2020-01-05 14:37:56 -0500

    init

--- cypress/integration/odysseyTests/sanity.spec.js
+++ cypress/integration/odysseyTests/sanity.spec.js
@@ -18,28 +18,33 @@ Cypress.Commands.add("login", (overrides = {}) => {
   };
   cy.request(options);
 });
-describe("login", () => {
-  it("should successfully log into our app", () => {
-    cy.login()
-      .then(resp => {
-        return resp.body;
-      })
-      .then(body => {
-        const { access_token, expires_in, id_token } = body;
-        const auth0State = {
-          nonce: "",
-          state: "some-random-state"
-        };
-        const callbackUrl = `http://localhost:3000/auth/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
-        cy.visit(callbackUrl, {
-          onBeforeLoad(win) {
-            win.document.cookie =
-              "com.auth0.auth.some-random-state=" + JSON.stringify(auth0State);
-          }
-        });
-        cy.get("#1-email").type(Cypress.env("test_username"));
-        cy.get(":input[type=password]").type(Cypress.env("test_password"));
-        cy.get("[name=submit]").click();
+
+Cypress.Commands.add("goToDashboard", (overrides = {}) => {
+  cy.login()
+    .then(resp => {
+      return resp.body;
+    })
+    .then(body => {
+      const { access_token, expires_in, id_token } = body;
+      const auth0State = {
+        nonce: "",
+        state: "some-random-state"
+      };
+      const callbackUrl = `http://localhost:3000/auth/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
+      cy.visit(callbackUrl, {
+        onBeforeLoad(win) {
+          win.document.cookie =
+            "com.auth0.auth.some-random-state=" + JSON.stringify(auth0State);
+        }
       });
+      cy.get("#1-email").type(Cypress.env("test_username"));
+      cy.get(":input[type=password]").type(Cypress.env("test_password"));
+      cy.get("[name=submit]").click();
+    });
+});
+
+describe("goDashboard", () => {
+  it("should land on the dashboard", () => {
+    cy.goToDashboard();
   });
 });
