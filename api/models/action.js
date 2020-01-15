8381b597ddcb50928a6ec482b54cfa9723114043
saresend <saresend@usc.edu>
2020-01-14 22:25:40 -0800

    adding migration and model

---
+++ api/models/action.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Action = sequelize.define(
+    "Action",
+    {
+      role: DataTypes.STRING(100),
+      name: DataTypes.STRING(100)
+    },
+    { tableName: "Actions" }
+  );
+  Action.associate = function(models) {};
+  return Action;
+};
