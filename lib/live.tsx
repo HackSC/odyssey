8f16dca0c66f27d528a150742105ea5db69dc12f
saresend <saresend@usc.edu>
2020-01-19 14:19:05 -0800

    removing console logs

--- lib/live.tsx
+++ lib/live.tsx
@@ -2,7 +2,6 @@ async function getCurrentTasks(req) {
   const urlRoute = req
     ? /* Serverside */ process.env.URL_BASE + "api/points/tasks"
     : /* Client */ "/api/points/tasks";
-  console.log(urlRoute);
   const results = await fetch(
     urlRoute,
     req
@@ -11,14 +10,12 @@ async function getCurrentTasks(req) {
         }
       : null
   );
-  console.log(results);
   return await results.json();
 }
 
 // CAN only be called from the client
 async function saveTask(newTask) {
   const urlRoute = "/api/points/tasks";
-  console.log(urlRoute);
   const result = await fetch(urlRoute, {
     method: "POST",
     headers: {
@@ -26,13 +23,10 @@ async function saveTask(newTask) {
     },
     body: JSON.stringify({ ...newTask, blocking: false, description: "" })
   });
-
-  console.log(result);
   return result.status === 200;
 }
 
 async function updateTask(updatedTask) {
-  console.log("New Value: " + JSON.stringify(updatedTask));
   const urlRoute = "/api/points/tasks";
   const result = await fetch(urlRoute, {
     method: "PUT",
@@ -42,7 +36,6 @@ async function updateTask(updatedTask) {
     body: JSON.stringify(updatedTask)
   });
   const jsonBody = await result.json();
-  console.log(jsonBody);
   return result.status === 200;
 }
 
