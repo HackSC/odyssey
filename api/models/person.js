c9698fb91cf2349631cec9d0a901181eb9e3514c
John Zeiders <zeiders@usc.edu>
2020-01-20 18:37:34 -0800

    [Server][Battlepass] Sets up tables and migrations (#209)
    * Sets up tables and migrations
    
    * Fixes prize association plurality

--- api/models/person.js
+++ api/models/person.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
     { tableName: "persons" }
   );
 
-  Person.associate = function(models) {};
+  Person.associate = function(models) {
+    Person.belongsTo(models.ProjectTeam);
+  };
 
   Person.removeAttribute("id");
   return Person;
