b7aef30afc492bc39f872d83fc6bd9853e1e9edf
Andreas Bigger <bigger@usc.edu>
2020-01-21 15:26:09 -0800

    hacksc fox 🦊

--- components/steps/LiveDashboard.tsx
+++ components/steps/LiveDashboard.tsx
@@ -4,30 +4,14 @@ import Router from "next/router";
 import * as Sentry from "@sentry/browser";
 import Link from "next/link";
 import QRCode from "react-qr-code";
-import { useIsMobile } from "../../lib/layouts";
 
 import { Flex, Column, Button } from "../../styles";
-
-import Check from "../../assets/check.svg";
-import GreenCheck from "../../assets/green_check.svg";
-import RedCheck from "../../assets/red_check.svg";
+import Step from "./Status";
+import HouseLogo from "../../assets/hackscFox.png";
 
 type Props = {
   profile: Profile;
   socialPosts: any;
-};
-
-const getQRCode = (profile: Profile): string => {
-  const { status, submittedAt } = profile;
-  if (!status || status === "unverified") {
-    return "Unverified";
-  }
-  if (status === "checkedIn") return "Checked In";
-  if (status === "verified" && !!submittedAt) {
-    return "Submitted";
-  }
-
-  return status[0].toUpperCase() + status.substring(1, status.length);
 };
 
 const getStatusLabel = (profile: Profile): string => {
@@ -69,23 +53,8 @@ const navigateTo = async (step: string) => {
   window.scrollTo(0, 0);
 };
 
-const getCheck = (profile: Profile) => {
-  const { status } = profile;
-
-  if (status === "accepted" || status === "confirmed") {
-    return <img src={GreenCheck} alt="Green Check" />;
-  } else if (status === "rejected" || status === "declined") {
-    return <img src={RedCheck} alt="Red Check" />;
-  } else {
-    return <img src={Check} alt="Check" />;
-  }
-};
-
-const StatusStep: React.FunctionComponent<Props> = props => {
+const LiveStep: React.FunctionComponent<Props> = props => {
   const { profile, socialPosts } = props;
-
-  const statusLabel = getStatusLabel(profile);
-  const isMobile = useIsMobile();
 
   if (profile.status === "confirmed") {
     return (
@@ -107,162 +76,62 @@ const StatusStep: React.FunctionComponent<Props> = props => {
         </InstructionsColumn>
       </Flex>
     );
-  }
-
-  return (
-    <Flex direction="column">
-      <h1>
-        {profile && profile.firstName
-          ? `Hey there, ${profile.firstName}!`
-          : "Hey there!"}
-      </h1>
-
-      <Status>
-        <Flex align="center" justify="space-between">
-          <Flex direction="row" align="center">
-            {getCheck(profile)}
-
-            <h2>Status</h2>
-          </Flex>
-
-          <Label>{statusLabel}</Label>
-        </Flex>
-
-        {profile && profile.status === "declined" && (
-          <StatusMessage>
-            We're sad to hear that you will not be attending HackSC 2020.{" "}
-            <b>If you would like to un-decline</b>, please do so on{" "}
-            <a href="/results">the results page.</a> If you have any additional
-            questions or comments, please let us know at{" "}
-            <a href="mailto:hackers@hacksc.com">hackers@hacksc.com</a>
-          </StatusMessage>
-        )}
-
-        {profile && profile.status === "accepted" && (
-          <StatusMessage>
-            You have been accepted to HackSC! Just one more step: please confirm
-            or decline your attendance by filling out{" "}
-            <a href="/results">this short form.</a>
-          </StatusMessage>
-        )}
-
-        {profile && profile.status === "rejected" && (
-          <StatusMessage>
-            Thank you for applying to HackSC. Unfortunately, we regret to inform
-            you that you have not been accepted to HackSC 2020. While we know
-            this isn't the news you'd like to hear, we wish you the best going
-            forward.
-          </StatusMessage>
-        )}
-      </Status>
-
+  } else if (profile.status === "checkedIn") {
+    return (
       <Flex justify="space-between" tabletVertical>
-        <Column flexBasis={63}>
-          <h2>Next Steps</h2>
-          <Steps>
-            <Step disabled={getStage(profile) > 1}>
-              <h3>1. Verify your e-mail</h3>
-              <p>
-                Make sure to check your e-mail and verify your account. If you
-                run into issues, log-out and log back in.
-              </p>
-            </Step>
-            <Step disabled={getStage(profile) > 2}>
-              <h3>2. Fill out an application</h3>
-              <p>
-                Answer a few questions to show why you want to be at HackSC
-                2020!
-              </p>
-
-              {getStage(profile) === 2 && (
-                <StepButton onClick={() => navigateTo("application")}>
-                  Fill out application
-                </StepButton>
-              )}
-            </Step>
-            <Step disabled={getStage(profile) === 4}>
-              <h3>
-                {profile.status === "accepted"
-                  ? "3) Confirm Attendance"
-                  : "3) View Results"}
-              </h3>
-              <p>
-                {profile.status === "accepted"
-                  ? "Congrats, you have been accepted to HackSC 2020. Please confirm/decline your attendance by January 1st"
-                  : "Come back soon and see your results."}
-              </p>
-              {getStage(profile) === 3 && (
-                <StepButton onClick={() => navigateTo("results")}>
-                  {profile.status === "accepted"
-                    ? "Confirm Attendance"
-                    : "View Results"}
-                </StepButton>
-              )}
-            </Step>
-          </Steps>
+        <Column flexBasis={40}>
+          <QRCodeWrapper>
+            <QRCode value={profile.userId} />
+          </QRCodeWrapper>
         </Column>
 
-        <DatesColumn flexBasis={35}>
-          <h2>Major Dates</h2>
+        <InstructionsColumn flexBasis={60}>
+          <CheckInTitle>
+            {profile && profile.firstName
+              ? `Hey there, ${profile.firstName}!`
+              : "Hey there!"}
+          </CheckInTitle>
 
-          <Dates>
-            <DateText>
-              <h3>Applications Open</h3>
-              <p>November 7th, 2019</p>
-            </DateText>
-
-            <DateText>
-              <h3>Applications Close</h3>
-              <p>December 8th, 2019</p>
-            </DateText>
-
-            <DateText>
-              <h3>HackSC 2020</h3>
-              <p>January 31, 2020</p>
-            </DateText>
-          </Dates>
-        </DatesColumn>
+          <Flex justify="space-between" tabletVertical>
+            <Column flexBasis={50}>
+              <CheckInTitle>House</CheckInTitle>
+              <Flex justify="space-between" tabletVertical>
+                <Column flexBasis={50}>
+                  <HouseImg src={HouseLogo} alt="House Logo" />
+                </Column>
+                <Column flexBasis={50}>
+                  <CheckInTitle>Points here</CheckInTitle>
+                </Column>
+              </Flex>
+            </Column>
+            <Column flexBasis={50}>
+              <CheckInTitle>You</CheckInTitle>
+              <Flex justify="space-between" tabletVertical>
+                <Column flexBasis={50}>
+                  <HouseImg src={HouseLogo} alt="House Logo" />
+                </Column>
+                <Column flexBasis={50}>
+                  <CheckInTitle>Points here</CheckInTitle>
+                </Column>
+              </Flex>
+            </Column>
+          </Flex>
+        </InstructionsColumn>
       </Flex>
-    </Flex>
-  );
-};
-
-type CircleIconProps = {
-  size: number;
-  bgColor: string;
-};
-
-const Status = styled.div`
-  padding: 48px;
-  margin: 16px 0 32px;
-  background: #ffffff;
-  border-radius: 4px;
-
-  h2 {
-    padding: 0;
-    margin-left: 16px;
+    );
   }
 
-  ${({ theme }) =>
-    theme.media.tablet`
-      padding: 32px;
-    `}
-`;
+  return <Step profile={profile} socialPosts={socialPosts} />;
+};
 
-const StatusMessage = styled.p`
-  margin-top: 24px;
+const HouseImg = styled.img`
+  width: 50px;
+  height: auto;
 `;
 
 const QRCodeWrapper = styled.div`
   width: 256px;
   margin: auto;
-`;
-
-const DatesColumn = styled(Column)`
-  ${({ theme }) =>
-    theme.media.tablet`
-      margin-top: 32px;
-    `}
 `;
 
 const InstructionsColumn = styled(Column)`
@@ -283,56 +152,8 @@ const CheckInInstructions = styled.p`
   font-weight: 300;
 `;
 
-const Label = styled.p`
-  margin: 0;
-  padding: 0;
-  font-weight: 600;
-`;
-
-const Steps = styled.div`
-  margin-top: 12px;
-`;
-
 type StepProps = {
   disabled?: boolean;
 };
 
-const Step = styled.div<StepProps>`
-  padding: 24px 36px;
-  margin-bottom: 24px;
-  background: #ffffff;
-  border-radius: 4px;
-
-  &:last-child {
-    margin-bottom: 0;
-  }
-
-  ${({ disabled }) => disabled && `opacity: 0.25;`}
-`;
-
-const StepButton = styled(Button)`
-  margin-top: 24px;
-`;
-
-const Dates = styled.div`
-  padding: 24px 24px;
-  margin: 16px 0 32px;
-  background: #ffffff;
-  border-radius: 4px;
-`;
-
-const DateText = styled.div`
-  margin-top: 36px;
-
-  :first-child {
-    margin-top: 0;
-  }
-
-  h3 {
-    font-weight: 600;
-    color: ${({ theme }) => theme.colors.peach};
-    padding-bottom: 4px;
-  }
-`;
-
-export default StatusStep;
+export default LiveStep;
