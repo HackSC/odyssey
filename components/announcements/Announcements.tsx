9a4b8b1f9d70d21f8d0ba94138f9ccb2bf5388fd
Andreas Bigger <bigger@usc.edu>
2020-01-23 17:08:36 -0800

    announcements component 📝

--- components/announcements/Announcements.tsx
+++ components/announcements/Announcements.tsx
@@ -7,36 +7,42 @@ const Announcements: React.FunctionComponent = () => {
   return (
     <>
       <h2>Announcements</h2>
-      <Column>
+      <AnnouncementsColumn>
         <AnnouncementsDiv>
           <CardWheel />
         </AnnouncementsDiv>
         <AnnouncementsFade />
-      </Column>
+      </AnnouncementsColumn>
     </>
   );
 };
 
+const AnnouncementsColumn = styled(Column)`
+  position: relative;
+`;
+
 const AnnouncementsDiv = styled.div`
   max-height: 20rem;
   overflow-y: scroll;
-  padding-bottom: 2rem;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const AnnouncementsFade = styled.div`
   content: "";
   position: absolute;
   z-index: 1;
-  bottom: 0;
+  bottom: -1rem;
   left: 0;
   pointer-events: none;
-  background-images: linear-gradient(
+  background-image: linear-gradient(
     to bottom,
     rgba(255, 255, 255, 0),
     white 90%
   );
   width: 100%;
-  max-height: 4rem;
+  height: 4rem;
 `;
 
 export default Announcements;
