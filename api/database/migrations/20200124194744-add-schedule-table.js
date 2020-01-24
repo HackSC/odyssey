43a2ec8d9ee4da10f12a6402a527e86a0ac8331d
saresend <saresend@usc.edu>
2020-01-24 12:03:01 -0800

    WIP

---
+++ api/database/migrations/20200124194744-add-schedule-table.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Events", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      startsAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      endsAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Events");
+  }
+};
---
+++ api/event.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const models = require("./models");
+const utils = require("./utils");
+const router = express.Router();
+const sequelize = require("sequelize");
+const Sentry = require("@sentry/node");
+
+/* This router supports superuser routes, such as updating the status of users */
+
+router.use(utils.authMiddleware);
+router.use(utils.requireAdmin);
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await models.Task.findAll();
+    return res.json({ tasks: tasks });
+  } catch (e) {
+    return res.json({ err: e });
+  }
+});
+
+router.get("/events", async (req, res) => {
+  // TODO: Make this work
+});
+
+module.exports = router;
