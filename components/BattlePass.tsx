2238a2ecaf60dcc19b18f28201245641ebdf5676
Wilhelm Willie <wilhelmwillie@gmail.com>
2020-01-30 14:23:30 -0800

    removed announcements, made more progress

--- components/BattlePass.tsx
+++ components/BattlePass.tsx
@@ -10,105 +10,139 @@ type Props = {
 type ItemProps = {
   premium?: boolean;
   prizeName?: string;
-  pointValue?: number;
   unlocked?: boolean;
+  currentTier: boolean;
   minimum: number;
 };
 
 const bpItem: React.SFC<ItemProps> = ({
   premium,
   prizeName,
-  pointValue,
   unlocked,
+  currentTier,
   minimum
 }) => {
   return (
     <TableTD
-      bgColor={unlocked ? "white" : "#757575"}
-      borderColor={unlocked ? "#FF8379" : "#757575"}
+      bgColor={premium ? "#E3BF5F" : "#7FBDE9"}
+      borderColor={currentTier ? "#FFFFFF" : "transparent"}
     >
       <LockImage>
-        {unlocked ? <BPOpenLock fill="#FF8379" /> : <BPLock />}
+        {unlocked ? <BPOpenLock fill="#FFFFFF" /> : <BPLock />}
       </LockImage>
       <MarginDiv>
         <PrizeItem>{prizeName}</PrizeItem>
-        <p>Min Points: {minimum}</p>
+        <PrizeMinPoints>
+          <b>Min Points:</b> {minimum}
+        </PrizeMinPoints>
       </MarginDiv>
+
+      {!unlocked && <LockedOverlay />}
     </TableTD>
   );
 };
 
+const useBattlepassItems = (bp: Battlepass, userPoints: number) => {
+  return useMemo(() => {
+    const premiumItems = bp.filter(item => {
+      return item.isPremium;
+    });
+
+    const basicItems = bp.filter(item => {
+      return !item.isPremium;
+    });
+
+    if (premiumItems && premiumItems.length > 0) {
+      premiumItems.reduce(
+        (total, item, idx) => {
+          let sum = idx === 0 ? 0 : total.total + item.pointValue;
+          item.unlocked = userPoints >= sum;
+          item.minimum = sum;
+          return { total: sum };
+        },
+        { total: 0 }
+      );
+    }
+
+    if (basicItems && basicItems.length > 0) {
+      basicItems.reduce(
+        (total, item, idx) => {
+          let sum = idx === 0 ? 0 : total.total + item.pointValue;
+          item.unlocked = userPoints >= sum;
+          item.minimum = sum;
+          return { total: sum };
+        },
+        { total: 0 }
+      );
+    }
+
+    const currentTier = useMemo(() => {
+      let i = 0;
+      for (i = 0; i < basicItems.length; i++) {
+        const item = basicItems[i];
+
+        if (userPoints < item.minimum) {
+          break;
+        }
+      }
+
+      return i - 1;
+    }, [basicItems, userPoints]);
+
+    const pointsTillNextTier = useMemo(() => {
+      if (currentTier === basicItems.length - 1) {
+        return 0;
+      } else {
+        console.log({ currentTier, l: basicItems.length });
+        return basicItems[currentTier + 1].minimum - userPoints;
+      }
+    }, [basicItems, currentTier]);
+
+    return {
+      basicItems,
+      premiumItems,
+      currentTier,
+      pointsTillNextTier
+    };
+  }, [bp]);
+};
 const BattlePass = ({ bp }: { bp: Battlepass }) => {
-  const userPoints = 80001;
+  const userPoints = 24000;
   const projSubmitted = false;
 
-  const premiumItems = bp.filter(item => {
-    return item.isPremium;
-  });
-
-  const basicItems = bp.filter(item => {
-    return !item.isPremium;
-  });
-
-  if (premiumItems && premiumItems.length > 0) {
-    premiumItems.reduce(
-      (total, item, idx) => {
-        let sum = idx === 0 ? 0 : total.total + item.pointValue;
-        item.unlocked = userPoints >= sum;
-        item.minimum = sum;
-        return { total: sum };
-      },
-      { total: 0 }
-    );
-  }
-  if (basicItems && basicItems.length > 0) {
-    basicItems.reduce(
-      (total, item, idx) => {
-        let sum = idx === 0 ? 0 : total.total + item.pointValue;
-        item.unlocked = userPoints >= sum;
-        item.minimum = sum;
-        return { total: sum };
-      },
-      { total: 0 }
-    );
-  }
-
-  const currentTier = useMemo(() => {
-    let i = 0;
-    for (i = 0; i < basicItems.length; i++) {
-      const item = basicItems[i];
-
-      if (userPoints < item.minimum) {
-        break;
-      }
-    }
-
-    return i - 1;
-  }, [basicItems, userPoints]);
-
-  const pointsTillNextTier = useMemo(() => {
-    if (currentTier === basicItems.length - 1) {
-      return 0;
-    } else {
-      console.log({ currentTier, l: basicItems.length });
-      return basicItems[currentTier + 1].minimum - userPoints;
-    }
-  }, [basicItems, currentTier]);
+  const {
+    basicItems,
+    premiumItems,
+    currentTier,
+    pointsTillNextTier
+  } = useBattlepassItems(bp, userPoints);
 
   const bptable = (
     <BPTable>
       <tbody>
         <RoundedTR>
           {basicItems
-            ? basicItems.map(item => {
-                return item ? bpItem({ premium: false, ...item }) : "";
+            ? basicItems.map((item, index) => {
+                return item
+                  ? bpItem({
+                      premium: false,
+                      ...item,
+                      currentTier: index === currentTier
+                    })
+                  : "";
               })
             : ""}
         </RoundedTR>
         <RoundedTR>
           {premiumItems
-            ? premiumItems.map(item => {
-                return item ? bpItem({ premium: true, ...item }) : "";
+            ? premiumItems.map((item, index) => {
+                return item
+                  ? bpItem({
+                      premium: true,
+                      ...item,
+                      currentTier: index === currentTier
+                    })
+                  : "";
               })
             : ""}
         </RoundedTR>
@@ -130,7 +164,7 @@ const BattlePass = ({ bp }: { bp: Battlepass }) => {
           <Column flexGrow={1}>
             <Flex direction="row" align="center">
               <Subheader>Tier</Subheader>
-              <BigNumber>{currentTier}</BigNumber>
+              <BigNumber>{currentTier + 1}</BigNumber>
             </Flex>
           </Column>
 
@@ -183,8 +217,8 @@ const BattlePassFade = styled.div`
 
 const LockImage = styled.div`
   position: absolute;
-  top: 2px;
-  left: 2px;
+  top: 4px;
+  left: 4px;
 `;
 
 const Header = styled.h2`
@@ -200,6 +234,10 @@ const Subheader = styled.h3`
 
 const BigNumber = styled.p`
   font-size: 32px;
+
+  ${({ theme }) => theme.media.tablet`
+    padding: 20px 0;
+  `}
 `;
 
 const Info = styled.div`
@@ -215,7 +253,12 @@ const MarginDiv = styled.div`
 `;
 
 const PrizeItem = styled.h4`
+  font-size: 18px;
   margin: auto;
+`;
+
+const PrizeMinPoints = styled.p`
+  margin-top: 12px;
 `;
 
 const BPTable = styled.table`
@@ -231,21 +274,29 @@ const Scrollable = styled.div`
 const RoundedTR = styled.tr`
   display: -webkit-inline-box;
   width: max-content;
+  padding-right: 4rem;
 `;
 
 const TableTD = styled.td<{ bgColor?: string; borderColor?: string }>`
   display: -webkit-inline-box;
   position: relative;
-  height: 100px;
+  height: 120px;
   background-color: ${({ bgColor = "white" }) => bgColor};
-  width: 120px;
-  border: solid;
+  width: 140px;
   padding: 10px;
   margin: 5px;
   vertical-align: middle;
-  border-width: 4px;
-  border-color: ${({ borderColor = "#757575" }) => borderColor};
   border-radius: 6px;
+  overflow: hidden;
+`;
+
+const LockedOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.65);
 `;
 
 export default BattlePass;
