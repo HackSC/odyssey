6b539c6e43fbe0009ede4bba81301de0836b584f
Wilhelm Willie <WilhelmWillie@gmail.com>
2020-01-31 14:41:14 -0800

    [fix] need to enable cors for public routes (#304)

--- api/public.js
+++ api/public.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const models = require("./models");
 const router = express.Router();
 
-router.get("/events/list", async (req, res) => {
+router.get("/events/list", cors(), async (req, res) => {
   try {
     const events = await models.Event.findAll();
     return res.json({ events });
