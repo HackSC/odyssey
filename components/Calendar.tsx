bdf16e44bab807a3fd1e4f30621836bd68adb09b
Andreas Bigger <bigger@usc.edu>
2020-01-31 14:01:05 -0800

    Andreas/user flow (#294)
    * qr code
    
    * livedashboard logic :rocket:
    
    * hacksc fox 🦊
    
    * hacksc fox 🦊
    
    * links and history 📝
    
    * progress
    
    * announcements component 📝
    
    * basic calendar component
    
    * a few additions
    
    * mild progress
    
    * locks
    
    * battlepass styling
    
    * battlepass styling
    
    * battlepass styling
    
    * useHouses hook
    
    * :rocke a lot of additions
    
    * dashboard
    
    * fox improvements 🦊:
    
    * fox improvements 🦊:
    
    * cleanup
    
    * fixed deepscan stuff
    
    * fixed deepscan stuff
    
    * final touches
    
    * some more style changes
    
    * styling again...
    
    * and again
    
    * last touches
    
    * decorative gold star
    
    * navbar
    
    * stuff
    
    * allEvents is not really working still
    
    * still an issue w events :unamused:
    
    * basic calendar component
    
    * convert time with moment... still acting up
    
    * remove useless logs
    
    * battlepass key dong
    
    * crappy calendar
    
    * some stuff
    
    * calendar some more
    
    * calendar
    
    * final calendar without coloring for types of events
    
    * description

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -7,6 +7,7 @@ import Timeline, {
   TimelineMarkers,
   TodayMarker
 } from "react-calendar-timeline";
+import { useEventsList } from "../lib/api-sdk/eventHooks";
 import "react-calendar-timeline/lib/Timeline.css";
 
 type Props = {};
@@ -21,61 +22,32 @@ type ItemProps = {
 
 const defaultTimeStart = moment()
   .startOf("day")
-  .toDate();
+  .add(moment().hour() - 2, "hour")
+  .valueOf();
+
 const defaultTimeEnd = moment()
   .startOf("day")
-  .add(12, "hour")
-  .toDate();
-
-const staticGroup = [
-  {
-    id: "1",
-    title: "HackSC-2020",
-    rightTitle: "HackSC-2020",
-    bgColor: "#000000"
-  }
-];
-
-const staticItems = [
-  {
-    id: "1",
-    group: "HackSC-2020",
-    title: "Event Title",
-    start: defaultTimeStart,
-    end: defaultTimeEnd,
-    canMove: false,
-    canResize: false,
-    className: "hacksc",
-    bgColor: "#000000",
-    selectedBgColor: "#FF8379",
-    color: "#1C1C1C",
-    itemProps: {
-      "data-tip": "this is a description"
-    }
-  }
-];
+  .add(moment().hour(), "hour")
+  .add(10, "hour")
+  .valueOf();
 
 const itemRenderer: React.FunctionComponent<ItemProps> = props => {
   const { item, itemContext, getItemProps, getResizeProps } = props;
   const { left: leftResizeProps, right: rightResizeProps } = getResizeProps();
-  const backgroundColor = itemContext.selected
-    ? itemContext.dragging
-      ? "red"
-      : item.selectedBgColor
-    : item.bgColor;
-  //const borderColor = itemContext.resizing ? "red" : item.color;
+
+  console.log(itemContext);
   return (
     <div
       {...getItemProps({
         style: {
-          backgroundColor,
+          backgroundColor: itemContext.selected ? "#f6f6f6" : "white",
           color: item.color,
           borderColor: "#FF8379",
           borderStyle: "solid",
-          borderWidth: 1,
+          borderWidth: 3,
           borderRadius: 4,
-          borderLeftWidth: itemContext.selected ? 3 : 1,
-          borderRightWidth: itemContext.selected ? 3 : 1
+          borderLeftWidth: 3,
+          borderRightWidth: 3
         },
         onMouseDown: () => {
           console.log("on item click", item);
@@ -86,10 +58,9 @@ const itemRenderer: React.FunctionComponent<ItemProps> = props => {
 
       <div
         style={{
-          height: itemContext.dimensions.height,
-          overflow: "hidden",
           paddingLeft: 3,
-          textOverflow: "ellipsis",
+          overflow: "scroll",
+          textOverflow: "ellipses",
           whiteSpace: "nowrap"
         }}
       >
@@ -102,34 +73,60 @@ const itemRenderer: React.FunctionComponent<ItemProps> = props => {
 };
 
 const Calendar: React.FunctionComponent<Props> = props => {
-  const keys = {
-    groupIdKey: "id",
-    groupTitleKey: "title",
-    groupRightTitleKey: "rightTitle",
-    itemIdKey: "id",
-    itemTitleKey: "title",
-    itemDivTitleKey: "title",
-    itemGroupKey: "group",
-    itemTimeStartKey: "start",
-    itemTimeEndKey: "end",
-    groupLabelKey: "title"
-  };
+  const { allEvents } = useEventsList({ defaultOnError: console.log });
+
+  const items = allEvents
+    ? allEvents.map((e: any) => {
+        let startTime = moment(e.startsAt)
+          .add(8, "hour")
+          .valueOf();
+        let endTime = moment(e.endsAt)
+          .add(8, "hour")
+          .valueOf();
+        return {
+          id: e.id + "" ?? "",
+          key: e.id + "" ?? "",
+          group: 1,
+          desc: e.description ?? "",
+          title: e.name + " - " + e.description ?? "Event",
+          start_time: startTime ?? "",
+          end_time: endTime ?? "",
+          canMove: false,
+          canResize: false,
+          className: "rct-item",
+          bgColor: "#FFFFFF",
+          selectedBgColor: "#FFFFFF",
+          color: "#1C1C1C",
+          itemProps: {
+            createdAt: e.createdAt ?? "",
+            updatedAt: e.updatedAt ?? ""
+          }
+        };
+      })
+    : [];
 
   return (
     <Timeline
-      groups={staticGroup}
-      items={staticItems}
-      keys={keys}
+      groups={[
+        {
+          id: 1,
+          title: "HackSC-2020",
+          rightTitle: "HackSC-2020",
+          bgColor: "#000000",
+          height: 400
+        }
+      ]}
+      items={items}
       sidebarWidth={0}
       itemTouchSendsClick={false}
-      stackItems
-      itemHeightRatio={0.75}
+      stackItems={true}
+      itemHeightRatio={0.8}
       showCursorLine
       canMove={false}
       canResize={false}
       defaultTimeStart={defaultTimeStart}
+      lineHeight={60}
       defaultTimeEnd={defaultTimeEnd}
-      lineHeight={300}
       itemRenderer={itemRenderer}
       style={{ width: "-webkit-fill-available", border: "hidden" }}
     >
@@ -142,7 +139,7 @@ const Calendar: React.FunctionComponent<Props> = props => {
           borderWidth: "4px"
         }}
       >
-        <SidebarHeader style={{ display: "none" }} />
+        <SidebarHeader />
         <DateHeader
           unit="day"
           labelFormat="dddd, LL"
