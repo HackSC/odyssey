a7bb8ea7eb6db55c81c9facda85f131c6978b6a9
Wilhelm Willie <WilhelmWillie@gmail.com>
2020-02-01 12:58:20 -0800

    [update] only show incomplete tasks with more than 0 points, and show description (#320)

--- components/IncompleteTasks.tsx
+++ components/IncompleteTasks.tsx
@@ -3,8 +3,12 @@ import styled from "styled-components";
 
 const IncompleteTasks = ({ incompleteTasks }: { incompleteTasks: Task[] }) => {
   const incompleteTasksList = useMemo(() => {
+    if (!incompleteTasks) {
+      return <p>No incomplete tasks</p>;
+    }
+
     const sort = (a: Task, b: Task) => b.points - a.points;
-    const filter = (a: Task) => a.isActive;
+    const filter = (a: Task) => a.isActive && a.points > 0;
 
     const sortedAndFilteredIncompleteTasks = incompleteTasks
       .filter(filter)
@@ -12,6 +16,7 @@ const IncompleteTasks = ({ incompleteTasks }: { incompleteTasks: Task[] }) => {
     return sortedAndFilteredIncompleteTasks.map(task => (
       <Task>
         <h3>{task.name}</h3>
+        <p>{task.description}</p>
         <b>Points Up For Grabs:</b> {task.points}
       </Task>
     ));
