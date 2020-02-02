b98a932a6005c4fa6342e8c51b8cbf2f6148752c
Wilhelm Willie <wilhelmwillie@gmail.com>
2020-02-02 10:56:17 -0800

    Update order of houses for winning

--- api/hackerLive.js
+++ api/hackerLive.js
@@ -263,11 +263,11 @@ router.get("/rafflePoints", async (req, res) => {
 
     if (houseId === 6) {
       totalRafflePoints += 1000;
-    } else if (houseId === 4) {
-      totalRafflePoints += 500;
-    } else if (houseId === 3) {
-      totalRafflePoints += 250;
     } else if (houseId === 5) {
+      totalRafflePoints += 500;
+    } else if (houseId === 4) {
+      totalRafflePoints += 250;
+    } else if (houseId === 3) {
       totalRafflePoints += 100;
     }
 
--- scripts/ticketRaffle.js
+++ scripts/ticketRaffle.js
@@ -69,11 +69,11 @@ async function getTicketsForProfile(userId) {
 
     if (houseId === 6) {
       totalRafflePoints += 1000;
-    } else if (houseId === 4) {
-      totalRafflePoints += 500;
-    } else if (houseId === 3) {
-      totalRafflePoints += 250;
     } else if (houseId === 5) {
+      totalRafflePoints += 500;
+    } else if (houseId === 4) {
+      totalRafflePoints += 250;
+    } else if (houseId === 3) {
       totalRafflePoints += 100;
     }
 
