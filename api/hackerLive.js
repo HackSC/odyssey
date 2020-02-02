1e5bb97a4c97ad870e94f6c0ce985c9c63834f28
saresend <saresend@usc.edu>
2020-02-01 20:55:40 -0800

    okay big changes now lol

--- api/hackerLive.js
+++ api/hackerLive.js
@@ -14,133 +14,133 @@ router.get("/battlepass", async (req, res) => {
       {
         id: "1",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "10 Raffle Tickets"
       },
       {
         id: "2",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "HackSC T-Shirt"
       },
       {
         id: "3",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "10 Raffle Tickets"
       },
       {
         id: "4",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "10 Raffle Tickets"
       },
       {
         id: "5",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "10 Raffle Tickets"
       },
       {
         id: "6",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "10 Raffle Tickets"
       },
       {
         id: "7",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "10 Raffle Tickets"
       },
       {
         id: "8",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "HackSC Stickers"
       },
       {
         id: "9",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "10 Raffle Tickets"
       },
       {
         id: "10",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "Drawstring Bag"
       },
       {
         id: "11",
         isPremium: false,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "12",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets & Travel Reimbursement!"
       },
       {
         id: "13",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "14",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "15",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "16",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "17",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "18",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "19",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "20",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets & Hacker Socks"
       },
       {
         id: "21",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets"
       },
       {
         id: "22",
         isPremium: true,
-        pointValue: 8000,
+        pointValue: 6000,
         prizeName: "100 Raffle Tickets & Hacker Hat"
       }
     ]
@@ -238,7 +238,7 @@ router.get("/rafflePoints", async (req, res) => {
     const totalPoints = parseInt(contributions[0].get("totalPoints"));
     const isPersonBPComplete = person.get("isBattlepassComplete") || 0;
 
-    const houseTier = Math.min(Math.floor(totalPoints / 7000), 10);
+    const houseTier = Math.min(Math.floor(totalPoints / 6000), 10);
     const tierPoints = [10, 10, 20, 30, 40, 50, 60, 60, 70, 70, 170];
     const premiumTierPoints = [
       100,
