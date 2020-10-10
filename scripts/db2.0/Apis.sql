f8ab11e77715fa8d6812f088d37c395cc54b98c6
Andreas Bigger <abigger87@gmail.com>
2020-10-10 16:02:58 -0700

    update api table

--- scripts/db2.0/Apis.sql
+++ scripts/db2.0/Apis.sql
@@ -3,6 +3,13 @@ CREATE TABLE Apis (
     id INT PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     description BLOB NOT NULL,
-    link VARCHAR(255) NOT NULL,
     FOREIGN KEY (id) REFERENCES MajorEvents(id)
+);
+
+-- Create an array of links by referencing Apis.id with api_id entry
+CREATE TABLE ApiLinks (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    api_id INT NOT NULL
 );
