06416c3e75dbac35dfaee95adcdf064858b1ff10
Andreas Bigger <abigger87@gmail.com>
2020-10-11 23:45:42 -0700

    sql scripts

---
+++ scripts/db2.0/create_majorevents.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D andreasdb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D chloedb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D jasondb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D maxdb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D katiedb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D rolanddb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D wilsondb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D testdb
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D production
+cat db2.0/MajorEvents.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p -D d4v1qrf33toril
+
+cat db2.0/insert_major_events.sql | mysql -u HackSCProd -h 159.89.138.243 -P 3306 -p
