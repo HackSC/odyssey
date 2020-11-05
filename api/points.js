780b54e3c41c9a9fcb03dbb28cca45ad4eede4d3
Roland <rolandtshen@gmail.com>
2020-11-04 17:54:59 -0800

    Remove debugging prints

--- api/points.js
+++ api/points.js
@@ -21,8 +21,6 @@ router.get("/tasks", async (req, res) => {
 });
 
 router.post("/tasks", async (req, res) => {
-  console.log("OTHER");
-  console.log(req.body);
   const allowedFields = new Set([
     "blocking",
     "description",
@@ -35,17 +33,8 @@ router.post("/tasks", async (req, res) => {
     "isPast"
   ]);
   const formInput = req.body;
-  console.log("forminput");
-  console.log(formInput);
-
-  console.log("in /tasks post api");
-  console.log(allowedFields);
-  console.log(formInput);
   for (let key of Object.keys(formInput)) {
-    console.log(key);
     if (!allowedFields.has(key)) {
-      console.log("NOT SUPPORTED FIELD");
-      console.log(key);
       return res.status(400).json({
         error: `${key} is not a supported field`
       });
@@ -55,20 +44,16 @@ router.post("/tasks", async (req, res) => {
     const result = await models.Task.create(req.body);
     return res.status(200).json({ result: result });
   } catch (e) {
-    console.log(e.message);
     return res.status(500).json({ error: e });
   }
 });
 
 router.put("/tasks", async (req, res) => {
-  console.log("hit update tasks");
   try {
-    // console.log(req.body);
     const updatedObj = req.body;
     if (updatedObj.createdAt === null) {
       updatedObj.createdAt = new Date();
     }
-    console.log(updatedObj);
     const taskId = updatedObj.id;
     delete updatedObj.id;
     await models.Task.update({ ...updatedObj }, { where: { id: taskId } });
