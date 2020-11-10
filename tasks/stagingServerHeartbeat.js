ce258a7df8eaf700737633cb00c9c13ccf6ba116
Andreas Bigger <abigger87@gmail.com>
2020-11-10 11:00:19 -0800

    Andreas/betteruptime heartbeat (#422)
    * betteruptime heartbeat
    
    * no export
    
    * server heartbeats
    
    * db heartbeats

--- tasks/stagingServerHeartbeat.js
+++ tasks/stagingServerHeartbeat.js
@@ -1,9 +1,11 @@
 const stagingServerHeartbeat = async () => {
-  if (process.env.HEROKU_APP_NAME.includes("staging")) {
+  if (process.env.URL_BASE && process.env.URL_BASE.includes("staging")) {
     fetch(
       "https://betteruptime.com/api/v1/heartbeat/u1po3JoaJEZmCmgtcWMD6n4x"
     ).then((res) => {
-      console.log("Successfully sent staging heartbeat to Better Uptime.");
+      console.log(
+        "Successfully sent staging heroku heartbeat to Better Uptime."
+      );
     });
   }
 };
