8cae409a0ee90d1c40b57b95544c0c09afd41dcf
Andreas Bigger <abigger87@gmail.com>
2020-11-10 18:42:06 +0000

    db heartbeats

--- cronjobs/cronjobs.js
+++ cronjobs/cronjobs.js
@@ -7,15 +7,20 @@ const productionServerHeartbeat = require("../tasks/productionServerHeartbeat");
 const stagingServerHeartbeat = require("../tasks/stagingServerHeartbeat");
 
 // * Uptime Database Heartbeats
-// TODO
+const productionDBHeartbeat = require("../tasks/productionDBHeartbeat");
+const stagingDBHeartbeat = require("../tasks/stagingDBHeartbeat");
 
 const scheduleCronJobs = () => {
   /* Upsert to send grid once an hour */
   cron.schedule("0 * * * *", sendgridSync);
 
-  // * Uptime Server Heartbeats
+  // * Better Uptime Server Heartbeats
   cron.schedule("0 0 * * * *", productionServerHeartbeat);
   cron.schedule("0 0 * * * *", stagingServerHeartbeat);
+
+  // * Better Uptime DB Heartbeats
+  cron.schedule("0 0 * * * *", productionDBHeartbeat);
+  cron.schedule("0 0 * * * *", stagingDBHeartbeat);
 };
 
 module.exports = scheduleCronJobs;
--- tasks/productionServerHeartbeat.js
+++ tasks/productionServerHeartbeat.js
@@ -1,9 +1,11 @@
 const productionServerHeartbeat = async () => {
-  if (process.env.HEROKU_APP_NAME.includes("production")) {
+  if (process.env.URL_BASE && process.env.URL_BASE.includes("dashboard")) {
     fetch(
       "https://betteruptime.com/api/v1/heartbeat/dZxGUymyrmjcLw1gjFFuBjMu"
     ).then((res) => {
-      console.log("Successfully sent production heartbeat to Better Uptime.");
+      console.log(
+        "Successfully sent production heroku heartbeat to Better Uptime."
+      );
     });
   }
 };
