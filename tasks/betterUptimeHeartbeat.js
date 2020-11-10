8ce7fee20420aa62ed69d75b00c676828675919f
Andreas Bigger <abigger87@gmail.com>
2020-11-10 17:06:46 +0000

    server heartbeats

--- tasks/betterUptimeHeartbeat.js
+++
@@ -1,8 +0,0 @@
-const betterUptimeHeartbeat = async () => {
-  let betteruptime_heartbeat_url =
-    "https://betteruptime.com/api/v1/heartbeat/rT5k5ZpriuRa27Lu1sMPJfAZ";
-
-  const res = await fetch(betteruptime_heartbeat_url);
-
-  return res;
-};
