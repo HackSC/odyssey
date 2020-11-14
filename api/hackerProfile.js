080909911571a665a429385ade402ef06d02396d
Andreas Bigger <abigger87@gmail.com>
2020-11-14 06:52:47 +0000

    fix missing u issue

--- api/hackerProfile.js
+++ api/hackerProfile.js
@@ -342,7 +342,7 @@ router.post("/undecline", async (req, res) => {
 
 router.get("/list", utils.requireDevelopmentEnv, async (req, res) => {
   try {
-    const profiles = await models.HackerProfile.findAll({ role: "admin" });
+    const profiles = await models.HackerProfile.findAll();
     return res.json({ profiles });
   } catch (e) {
     return res.status(500).json({ error: e });
