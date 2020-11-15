635d2c4659c0093a95e5bc2a7dd36b41a6cf76c4
Andreas Bigger <abigger87@gmail.com>
2020-11-14 22:00:44 -0800

    confetti for reviews and a bunch of fixes

--- lib/admin.tsx
+++ lib/admin.tsx
@@ -4,7 +4,7 @@ export async function getProfiles(query) {
     : "api/admin/profiles?query=" + query;
 
   const response = await fetch(fetchUrl, {
-    method: "GET"
+    method: "GET",
   });
 
   const payload = await response.json();
@@ -22,9 +22,9 @@ export async function updateProfileRole(email, role) {
   const response = await fetch(fetchUrl, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email: email, role: role })
+    body: JSON.stringify({ email: email, role: role }),
   });
 
   return response;
@@ -39,13 +39,31 @@ export async function getReviewHistory(req) {
     fetchUrl,
     req
       ? {
-          headers: req.headers
+          headers: req.headers,
         }
       : null
   );
 
   const payload = await response.json();
   return payload.reviews;
+}
+
+export async function getTotalReviewHistory(req) {
+  const fetchUrl = process.env.URL_BASE
+    ? process.env.URL_BASE + "api/admin/eligibleProfiles"
+    : "api/admin/eligibleProfiles";
+
+  const response = await fetch(
+    fetchUrl,
+    req
+      ? {
+          headers: req.headers,
+        }
+      : null
+  );
+
+  const payload = await response.json();
+  return payload;
 }
 
 export async function getHackerProfileForReview(req) {
@@ -57,7 +75,7 @@ export async function getHackerProfileForReview(req) {
     fetchUrl,
     req
       ? {
-          headers: req.headers
+          headers: req.headers,
         }
       : null
   );
@@ -77,9 +95,9 @@ export async function submitReview(review) {
   const response = await fetch(fetchUrl, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(review)
+    body: JSON.stringify(review),
   });
   return response;
 }
