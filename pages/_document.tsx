e8e2d2ba7f270696413a3d8a0f2246381e756454
Max Leiter <mleiter@usc.edu>
2020-11-29 12:19:43 -0800

    Bug fixes, refactoring, and application updates for 2021
    Co-authored-by: Andreas Bigger <abigger87@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
           <script
             src="https://betteruptime.com/widgets/announcement.js"
@@ -44,7 +44,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
