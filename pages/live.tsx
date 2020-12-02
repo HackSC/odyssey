eae18d43b512af960be80fae4e5c94e6eb6fca84
Andreas Bigger <abigger87@gmail.com>
2020-12-02 08:14:42 -0800

    a bunch of admin enhancements and visibility with live dashboard

--- pages/live.tsx
+++ pages/live.tsx
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Sentry from "@sentry/browser";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 import {
   Head,
   Navbar,
-  Hero,
   Footer,
   Countdown,
-  LinkToSchedule,
-  LinksAndTasks
+  QRCode,
+  HouseProgress,
+  BattlePass,
+  LinkScroll,
+  IncompleteTasks,
 } from "../components";
 
-import { Container } from "../styles";
+import { Container, Flex, CenteredColumn, Column } from "../styles";
 import { generatePosts } from "../lib/referrerCode";
+
+import {
+  useBattlepass,
+  useAllHouseInfo,
+  useAllTasks,
+} from "../lib/api-sdk/hackerLiveHooks";
 
 import {
   handleLoginRedirect,
   getProfile,
-  handleAdminRedirect,
-  handleVolunteerRedirect,
-  handleSponsorRedirect,
-  handleDashboardRedirect,
   handleApplicationRedirect,
 } from "../lib/authenticate";
+
 import hackathonConstants from "../lib/hackathonConstants";
+import styled from "styled-components";
+import Events from "../components/events/Events";
+import { useEventsList } from "../lib/api-sdk/eventHooks";
 
 const Live = ({ profile, houses, socialPosts }) => {
+  // * External Hooks
+  const { allHouses } = useAllHouseInfo({});
+  const { battlepass } = useBattlepass({ defaultOnError: console.log });
+  const { allEvents } = useEventsList({ defaultOnError: console.log });
+  const { allTasks } = useAllTasks({
+    defaultOnError: console.log,
+  });
+
+  // * Local Hooks
+  const [alert, setAlert] = useState(false);
+
   return (
     <>
       <Head title="HackSC Odyssey - Live Dashboard" />
@@ -41,8 +60,53 @@ const Live = ({ profile, houses, socialPosts }) => {
       )}
       <Container>
         <Countdown />
-        {/* <LinkToSchedule /> */}
-        <LinksAndTasks />
+        <PaddedFlex
+          style={!alert ? { paddingTop: "2em" } : {}}
+          justify="space-between"
+          tabletVertical
+        >
+          <Column flexBasis={40}>
+            <QRCode profile={profile} />
+          </Column>
+
+          <InstructionsColumn flexBasis={60}>
+            <CheckInTitle>
+              {profile && profile.firstName
+                ? `Hey there, ${profile.firstName}!`
+                : "Hey there!"}
+            </CheckInTitle>
+
+            <Flex justify="space-between" tabletVertical>
+              <CenteredColumn flexBasis={100}>
+                <HouseProgress houses={allHouses} />
+              </CenteredColumn>
+            </Flex>
+          </InstructionsColumn>
+        </PaddedFlex>
+        <PaddedFlex justify="space-between" tabletVertical>
+          <MarginedColumn style={{ overflowX: "scroll" }} flexBasis={100}>
+            <BattlePass
+              bp={battlepass}
+              userPoints={battlepass.reduce((a, b) => a + b.pointValue, 0)}
+              projSubmitted={false}
+            />
+            <UnlockMessage>
+              Premium tier prizes are unlocked when a team submits their project
+            </UnlockMessage>
+          </MarginedColumn>
+        </PaddedFlex>
+        <PaddedFlex tabletVertical>
+          <Events events={allEvents} />
+        </PaddedFlex>
+        <PaddedFlex justify="space-between" tabletVertical>
+          <MarginedColumn flexBasis={35}>
+            <LinkScroll />
+          </MarginedColumn>
+          <MarginedColumn flexBasis={65}>
+            <CheckInTitle>All Tasks</CheckInTitle>
+            <IncompleteTasks incompleteTasks={allTasks} />
+          </MarginedColumn>
+        </PaddedFlex>
       </Container>
       <Footer />
     </>
@@ -57,31 +121,19 @@ Live.getInitialProps = async ({ req }) => {
   if (!profile) {
     handleLoginRedirect(req);
   }
-  // else if (profile.role == "admin") {
-  //   handleAdminRedirect(req);
-  // } else if (profile.role == "volunteer") {
-  //   handleVolunteerRedirect(req);
-  // } else if (profile.role == "sponsor") {
-  //   handleSponsorRedirect(req);
-  // }
 
-  if (profile && !hackathonConstants.showLive) {
+  let is_super_user =
+    profile.role == "admin" ||
+    profile.role == "volunteer" ||
+    profile.role == "sponsor";
+
+  if (profile && !is_super_user && !hackathonConstants.showLive) {
     // Redirect user to dashboard if they are logged in
     handleApplicationRedirect(req);
-  }
-
-  if (profile && !hackathonConstants.showLive) {
-    // Redirect user to dashboard if they are logged in
-    handleApplicationRedirect(req);
-  }
-
-  if (profile && profile.status == "checkedIn") {
-    //const houseInfo = await getHouseInfo(req, 1);
-    //console.log(houseInfo);
   }
 
   if (typeof window !== "undefined") {
-    Sentry.configureScope(function(scope) {
+    Sentry.configureScope(function (scope) {
       scope.setExtra("profile", profile);
     });
   }
@@ -94,8 +146,35 @@ Live.getInitialProps = async ({ req }) => {
   return {
     houses,
     profile,
-    socialPosts
+    socialPosts,
   };
 };
+
+const PaddedFlex = styled(Flex)`
+  padding: 1em;
+`;
+
+const MarginedColumn = styled(Column)`
+  margin: 1rem;
+`;
+
+const InstructionsColumn = styled(Column)`
+  margin: 0 15px;
+  ${({ theme }) =>
+    theme.media.tablet`
+    margin-top: 32px;
+    text-align: center;
+  `}
+`;
+
+const CheckInTitle = styled.h2`
+  text-align: center;
+  padding-bottom: 15px;
+`;
+
+const UnlockMessage = styled.p`
+  font-size: 18px;
+  font-style: italic;
+`;
 
 export default Live;
