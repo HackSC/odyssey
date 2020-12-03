6f20f7f89bf7347d5fd927e1f3cb94b1a6b62d31
Max Leiter <mleiter@usc.edu>
2020-12-02 19:21:21 -0800

    Slack announce improvements (#449)
    * Slack bot announcements on app submit
    
    * Some null checks + Slack blocks
    
    Co-authored-by: Andreas Bigger <abigger87@gmail.com>

--- pages/api/slack/appSubmit.ts
+++ pages/api/slack/appSubmit.ts
@@ -15,13 +15,50 @@ const app = new App({
 });
 
 export default async (req, res) => {
+  if (!process.env.SLACK_BOT_TOKEN || !process.env.SIGNING_SECRET) {
+    res.json({
+      status: "error",
+      message: "Missing required environment variables.",
+    });
+  }
+
   const { firstName, lastName, school, year, ...body } = req.body;
+  if (!firstName || !lastName || !school) {
+    res.json({
+      status: "error",
+      message:
+        "Missing required information to send application to Slack. Verify app is valid?",
+    });
+  }
+
   await app.start(3032);
   try {
     await app.client.chat.postMessage({
       token: process.env.SLACK_BOT_TOKEN,
       channel: "C01FUMML1JA", // #2021-app-announcements
-      text: `${firstName} ${lastName} (${year}) has submit an application!`,
+      text: ``,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "plain_text",
+            text: `${firstName} ${lastName} has submit their application!`,
+            emoji: true,
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: `A ${year} at ${school} studying ${body.major}`,
+            },
+          ],
+        },
+      ],
     });
   } catch (error) {
     console.error(error);
