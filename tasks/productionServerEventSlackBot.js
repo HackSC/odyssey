ba822f5c84db817fc06bf505f18f394bbc1a2ce5
Andreas Bigger <abigger87@gmail.com>
2020-12-03 00:04:09 -0800

    ready for prod

--- tasks/productionServerEventSlackBot.js
+++ tasks/productionServerEventSlackBot.js
@@ -1,45 +1,49 @@
-const { WebClient } = require("@slack/web-api");
+const { App } = require("@slack/bolt");
 
-const SendSlackMessage = async (e, starting_phrase) => {
-  const bot = new WebClient(process.env.SLACK_BOT_TOKEN);
-  await bot.chat.postMessage({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel: process.env.SLACK_ANNOUNCEMENTS_CHANNEL
-      ? process.env.SLACK_ANNOUNCEMENTS_CHANNEL
-      : "C01FUMML1JA", // * #2021-app-announcements
-    text: ``,
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "plain_text",
-          text: `${e.name} ${starting_phrase}!`,
-          emoji: true,
+const SendSlackMessage = async (e, app, starting_phrase) => {
+  try {
+    await app.client.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: process.env.SLACK_ANNOUNCEMENTS_CHANNEL
+        ? process.env.SLACK_ANNOUNCEMENTS_CHANNEL
+        : "C01FUMML1JA", // * #2021-app-announcements
+      text: ``,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "plain_text",
+            text: `${e.name} ${starting_phrase}!`,
+            emoji: true,
+          },
         },
-      },
-      {
-        type: "context",
-        elements: [
-          {
-            type: "mrkdwn",
-            text: `Description: ${e.description}\n`,
-          },
-          {
-            type: "mrkdwn",
-            text: `Starting At: ${e.startsAt}\n`,
-          },
-          {
-            type: "mrkdwn",
-            text: `Ends At: ${e.endsAt}\n`,
-          },
-        ],
-      },
-      {
-        type: "divider",
-      },
-    ],
-  });
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: `Description: ${e.description}\n`,
+            },
+            {
+              type: "mrkdwn",
+              text: `Starting At: ${e.startsAt}\n`,
+            },
+            {
+              type: "mrkdwn",
+              text: `Ends At: ${e.endsAt}\n`,
+            },
+          ],
+        },
+        {
+          type: "divider",
+        },
+      ],
+    });
+  } catch (msg_error) {
+    console.error("Could not send slackbot message: ", msg_error);
+  }
 };
+
 const productionServerSlackBot = async () => {
   // * Null check slackbot tokens
   if (!process.env.SLACK_BOT_TOKEN || !process.env.SIGNING_SECRET) {
@@ -49,6 +53,13 @@ const productionServerSlackBot = async () => {
     process.env.URL_BASE.includes("dashboard")
   ) {
     try {
+      const app = new App({
+        token: process.env.SLACK_BOT_TOKEN,
+        signingSecret: process.env.SIGNING_SECRET,
+      });
+
+      await app.start(3035);
+
       // * Fetch event schedule from odyssey API
       fetch("https://dashboard.hacksc.com/api/public/events/list")
         .then((res) => res.json())
@@ -70,13 +81,13 @@ const productionServerSlackBot = async () => {
               event_start_time - curr_date_min_10 > -1 &&
               event_start_time - curr_date_min_10 < 1
             ) {
-              SendSlackMessage(e, "starting in 10 minutes");
+              SendSlackMessage(e, app, "starting in 10 minutes");
             }
             if (
               event_start_time - curr_time > -1 &&
               event_start_time - curr_time < 1
             ) {
-              SendSlackMessage(e, "starts now");
+              SendSlackMessage(e, app, "starts now");
             }
           });
         });
