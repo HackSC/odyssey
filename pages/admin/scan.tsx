aae8bea5b641e9cb997a08993bcad40cbfda44bf
Andreas Bigger <abigger87@gmail.com>
2020-12-08 13:40:05 -0800

    a ton of fixes

--- pages/admin/scan.tsx
+++ pages/admin/scan.tsx
@@ -165,7 +165,6 @@ const Scan = ({ admin_profile, tasks }: Props) => {
       return;
     }
 
-    console.log(dispatchBody);
     const scanResponse = await liveDispatchFetch(dispatchBody);
 
     if (!scanResponse.error) {
