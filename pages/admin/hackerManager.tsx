8b0986a54b40261a2e604a6ca934cdb0f3c137c1
Max Leiter <mleiter@usc.edu>
2020-12-24 15:04:44 -0800

    Improve admin visibility message to not include null firstName/lastName (#506)

--- pages/admin/hackerManager.tsx
+++ pages/admin/hackerManager.tsx
@@ -263,17 +263,23 @@ const hackerManager = ({ profile }) => {
       let user_email = profile ? profile.email : "";
       let start_and_end_date =
         new Date(new Date().getTime() - 480 * 1000 * 60).toISOString() + "";
-      let slack_result = await sendSlackMessage(
-        ":file_folder: Hacker CSV exported (/admin/hackerManager) by " +
-          firstName +
-          ", " +
-          lastName +
-          ", " +
-          user_email,
-        "Number hackers exported: " + (csvs[0].length - 1),
-        start_and_end_date,
-        start_and_end_date
-      );
+      let slack_result: Response;
+
+      if (!firstName) {
+        slack_result = await sendSlackMessage(
+          `:file_folder: Hacker CSV exported (/admin/hackerManager) by ${user_email}`,
+          "Number hackers exported: " + (csvs[0].length - 1),
+          start_and_end_date,
+          start_and_end_date
+        );
+      } else {
+        slack_result = await sendSlackMessage(
+          `:file_folder: Hacker CSV exported (/admin/hackerManager) by ${firstName} ${lastName} (${user_email})`,
+          "Number hackers exported: " + (csvs[0].length - 1),
+          start_and_end_date,
+          start_and_end_date
+        );
+      }
     } catch (err) {
       setMessage(err.message);
       return;
