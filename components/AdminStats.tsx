3a81e7675ff48c7bf539077a8dddb2c6afc3ee5a
Andreas Bigger <abigger87@gmail.com>
2020-12-30 17:31:03 -0800

    added superadmin to users allowed to view statistics

--- components/AdminStats.tsx
+++ components/AdminStats.tsx
@@ -12,7 +12,7 @@ const AdminStats = ({ profile, showtitle = true }) => {
     });
   }, []);
 
-  if (profile.role != "admin") return <></>;
+  if (profile.role !== "admin" || profile.role !== "superadmin") return <></>;
   return (
     <Container>
       {showtitle ? <h2>Statistics</h2> : ""}
