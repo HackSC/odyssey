d4bd58ad5a1e9d5f1074983a1ca3f75514264d34
Roland <rolandtshen@gmail.com>
2021-02-09 11:32:47 -0800

    add extra check in component

--- api/linkSlack.js
+++ api/linkSlack.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const models = require("./models");
 const router = express.Router();
+const sequelize = require("sequelize");
 
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -33,10 +34,40 @@ router.post("/", async (req, res) => {
           slackProfile: link.id,
         })
         .then((updatedHacker) => {
-          console.log(updatedHacker);
+          console.log("hacker updated");
         });
     }
 
+    const [pointsProfile, isCreated] = await models.Person.findOrCreate({
+      where: { identityId: hacker.userId },
+      defaults: { isBattlepassComplete: false },
+    });
+
+    if (!isCreated) {
+      return res.json({ text: "You're already checked in" });
+    }
+
+    const minHouse = await models.House.findOne({
+      attributes: [
+        ["id", "id"],
+        ["name", "name"],
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM persons where persons.houseId = House.id)"
+          ),
+          "personCount",
+        ],
+      ],
+      order: [[sequelize.literal("personCount"), "ASC"]],
+    });
+
+    hacker.status = "checkedIn";
+    await hacker.save();
+
+    pointsProfile.houseId = minHouse.id;
+    await pointsProfile.save();
+
+    console.log(pointsProfile);
     return res.json({ text: "Thanks for checking in! You're ready to go." });
   } else {
     return res.json({ text: "Invalid Slack user." });
