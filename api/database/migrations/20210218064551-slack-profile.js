2bbdd3dc5c832a97762d065c609aaa880b3375be
Roland <rolandtshen@gmail.com>
2021-02-18 15:21:17 -0800

    fix migration

--- api/database/migrations/20210218064551-slack-profile.js
+++ api/database/migrations/20210218064551-slack-profile.js
@@ -2,32 +2,36 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("LinkedSlacks", {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true,
-      },
-      slackId: {
-        type: Sequelize.STRING,
-      },
-      userId: {
-        type: Sequelize.STRING,
-        references: {
-          model: "HackerProfiles",
-          key: "userId",
+    return Promise.all([
+      queryInterface.createTable("LinkedSlacks", {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
         },
-      },
-      devPostSubmission: {
-        type: Sequelize.STRING,
-      },
-      checkInTime: {
-        type: Sequelize.DATE,
-      },
-    });
+        slackId: {
+          type: Sequelize.STRING,
+        },
+        userId: {
+          type: Sequelize.STRING,
+          references: {
+            model: "HackerProfiles",
+            key: "userId",
+          },
+        },
+        devPostSubmission: {
+          type: Sequelize.STRING,
+        },
+        checkInTime: {
+          type: Sequelize.DATE,
+        },
+      })
+    ])
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("LinkedSlacks");
+    return Promise.all([
+      queryInterface.dropTable("LinkedSlacks")
+    ])
   },
 };
