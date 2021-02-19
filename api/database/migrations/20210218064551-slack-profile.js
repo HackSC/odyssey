d244a36f1e11d3b331b3bce2aa9d162fbcf574b6
Andreas Bigger <abigger87@gmail.com>
2021-02-18 18:16:15 -0800

    quick fix (#592)

--- api/database/migrations/20210218064551-slack-profile.js
+++ api/database/migrations/20210218064551-slack-profile.js
@@ -2,30 +2,37 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("LinkedSlacks", {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true,
-      },
-      slackId: {
-        type: Sequelize.STRING,
-      },
-      userId: {
-        type: Sequelize.STRING,
-        references: {
-          model: "HackerProfiles",
-          key: "userId",
+    return queryInterface.createTable(
+      "LinkedSlacks",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
+        },
+        slackId: {
+          type: Sequelize.STRING,
+        },
+        userId: {
+          type: Sequelize.STRING,
+          references: {
+            model: "HackerProfiles",
+            key: "userId",
+          },
+        },
+        devPostSubmission: {
+          type: Sequelize.STRING,
+        },
+        checkInTime: {
+          type: Sequelize.DATE,
         },
       },
-      devPostSubmission: {
-        type: Sequelize.STRING,
-      },
-      checkInTime: {
-        type: Sequelize.DATE,
-      },
-    });
+      {
+        charset: "utf8mb4",
+        collate: "utf8mb4_bin", // * New Databases: utf8mb4_0900_ai_ci
+      }
+    );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable("LinkedSlacks");
